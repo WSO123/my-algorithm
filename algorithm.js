f3990fbea5d024703f64e08c13b6a3fa7044763f
Song <50765774+WSO123@users.noreply.github.com>
2025-01-07 08:54:48 +0000

    约瑟夫问题

--- algorithm.js
+++ algorithm.js
@@ -3469,3 +3469,41 @@ function isStraight(nums) {
     }
     return  max - min < 5 && zeroCount + set.size === 5;
 }
+
+// 78、圆圈中最后剩下的数字，约瑟夫问题
+// 0,1,・・・,n-1 这 n 个数字排成一个圆圈，从数字 0 开始，每次从这个圆圈里删除第 m 个数字（删除后从下一个数字开始计数）。求出这个圆圈里剩下的最后一个数字。
+// 例如，0、1、2、3、4 这 5 个数字组成一个圆圈，从数字 0 开始每次删除第 3 个数字，则删除的前 4 个数字依次是 2、0、4、1，最后剩下的数字是 3。
+// 思路1: 构建环形链表，去遍历删除
+function lastNumber(n, m) {
+    let head = new ListNode(0)
+    let prev = head
+    // 构建环形链表
+    for(let i = 1; i < n; i++) {
+        let node = new ListNode(i)
+        prev.next = node
+        prev = prev.next
+    }
+    prev.next = head
+    let cur = head
+
+    while(n > 1) {
+        // 移动到第m - 1个节点
+        for(let i = 1; i < m - 1; i++) {
+            cur = cur.next
+        }
+        // 删除 cur.next 节点
+        cur.next = cur.next.next
+        // cur 指向下一个节点
+        cur = cur.next
+        n--
+    }
+    return cur.val
+}
+// 思路2: 数学公式，约瑟夫问题的递推公式是f(n, m) = (f(n-1, m) + m) % n， 
+function lastNumber(n, m) {
+    let last = 0; // 基本情况：当只有一个人时，返回位置 0
+    for (let i = 2; i <= n; i++) {
+        last = (last + m) % i;
+    }
+    return last;
+}
