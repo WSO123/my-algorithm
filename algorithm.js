03bbd01e1f7dc89b6a5e1da8bb7fb894532bdc56
肉松 <song1996510@gmail.com>
2025-02-05 11:28:56 +0800

    search

--- algorithm.js
+++ algorithm.js
@@ -10377,9 +10377,50 @@ function maxListSum(nums) {
     return cur
 }
 
-// 217、接雨水 
+// 217、接雨水 难度：困难
+// 给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水
+// 思路： 与126题类似，可以用单调栈做
+function trap(height) {
+
+}
 
 // 218、搜索旋转排序数组
+// 整数数组 nums 按升序排列，数组中的值 互不相同 。
+// 在传递给函数之前，nums 在预先未知的某个下标 k（0 <= k < nums.length）上进行了 旋转，使数组变为 [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]]（下标 从 0 开始 计数）。例如， [0,1,2,4,5,6,7] 在下标 3 处经旋转后可能变为 [4,5,6,7,0,1,2] 。
+// 给你 旋转后 的数组 nums 和一个整数 target ，如果 nums 中存在这个目标值 target ，则返回它的下标，否则返回 -1 。
+// 你必须设计一个时间复杂度为 O(log n) 的算法解决此问题
+// 思路： 典型的二分查找
+function search(nums, target) {
+    let left = 0
+    let right = nums.length - 1
+    while(left <= right) {
+        let mid = (left + right) >> 1
+
+        // 如果找到目标，直接返回
+        if(nums[mid] === target) {
+            return mid
+        }
+
+        // 左半边有序
+        if(nums[left] <= nums[mid]) {
+            // 如果目标在左半部分
+            if(nums[left] <= target && target < nums[mid]) {
+                right = mid - 1
+            } else {
+                left = mid + 1
+            }
+        } else {
+            // 如果目标在右半部分
+            if(nums[mid] < target && target <= nums[right]) {
+                left = mid + 1
+            } else {
+                right = mid - 1
+            }
+        }
+    }
+
+    return -1
+}
 
 // 219、岛屿数量
 
