7e9913a6f939e02947ca195eb9bd6cf4c1bbe046
肉松 <song1996510@gmail.com>
2025-02-05 11:08:35 +0800

    maxListSum

--- algorithm.js
+++ algorithm.js
@@ -10346,6 +10346,36 @@ function lengthOfLongestSubstring(s) {
 // 思路： 和34题c是同一题
 
 // 216、最大子序和
+// 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+// 示例: 输入: [-2,1,-3,4,-1,2,1,-5,4],输出: 6，解释: 连续子数组 [4,-1,2,1] 的和最大，为 6
+// 思路：动态规划
+//   假设f(i)表示以第i个元素结尾的最大子序和
+//   那么f(i) = max(f(i-1) + nums[i], nums[i])
+//      当前元素要么加入前面的最大和，要么自己作为新的子序列的起点
+function maxListSum(nums) {
+    const len = nums.length
+    // 初始化状态数组
+    const fn = new Array(len).fill(0)
+    fn[0] = nums[0] // 初始化为第一个元素
+    for(let i = 1; i < len; i++) {
+        fn[i] = Math.max(fn[i - 1] + nums[i], nums[i])
+    }
+
+    return fn[len - 1]
+}
+// 优化空间复杂度
+function maxListSum(nums) {
+    const len = nums.length
+
+    let prev = nums[0] // 初始化为第一个元素
+    let cur = nums[0]
+    for(let i = 1; i < len; i++) {
+        prev = Math.max(prev + nums[i], nums[i])
+        cur = Math.max(cur, prev)
+    }
+
+    return cur
+}
 
 // 217、接雨水 
 
