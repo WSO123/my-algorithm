065a4a7a5dea6a1fa8af92433f917fb4d053b21e
雾淞 <1552728061@qq.com>
2025-02-24 23:08:57 +0800

    pathSum

--- algorithm.js
+++ algorithm.js
@@ -10794,6 +10794,33 @@ function longestPalindrome(s) {
 }
 
 // 226、路径总和 II
+// 给你二叉树的根节点 root 和一个整数目标和 targetSum ，找出所有 从根节点到叶子节点 路径总和等于给定目标和的路径。
+// 叶子节点 是指没有子节点的节点
+// 思路：深度优先遍历 + 回溯
+function pathSum(root, targetSum) {
+    const res = []
+    const dfs = (node, target, path) => {
+        if(!node) {
+            return
+        }
+
+        // 当前节点加入path
+        path.push(node.val)
+        if(!node.left && !node.right && target === node.val) {
+            res.push([...path])
+        } else {
+            dfs(node.left, target - node.val, path)
+            dfs(node.right, target - node.val, path)
+        }
+
+        // 回溯
+        path.pop()
+    }
+
+    dfs(root, targetSum, [])
+    return res
+}
+
 
 // 227、反转链表 II
 
