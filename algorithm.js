a97a05328ddacecf2e046010a9947d1a1251a699
雾淞 <1552728061@qq.com>
2025-05-24 21:11:29 +0800

    divingBoard

--- algorithm.js
+++ algorithm.js
@@ -12844,6 +12844,27 @@ function maxAliveYear(birth, death) {
 }
 
 // 314、跳水板
+// 你正在使用一堆木板建造跳水板。有两种类型的木板，其中长度较短的木板长度为shorter，长度较长的木板长度为longer。你必须正好使用k块木板。
+// 编写一个方法，生成跳水板所有可能的长度。
+// 返回的长度需要从小到大排列。
+// 示例：
+// 输入：
+// shorter = 1
+// longer = 2
+// k = 3
+// 输出： {3,4,5,6}
+function divingBoard(shorter, longer, k) {
+    if (k === 0) return [];
+    if (shorter === longer) return [shorter * k];
+    const res = [];
+    for (let i = 0; i <= k; i++) {
+        // 计算当前长度
+        // 短的 * (k - i) + 长的 * i
+        // i 从0开始，到k结束，确保短的从多数到少数，长的从少数到多数，这样就能保证放进去的数量是从小到大
+        res.push(shorter * (k - i) + longer * i);
+    }
+    return res;
+}
 
 // 316、平分正方形
 
